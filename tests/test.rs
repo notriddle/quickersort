6ce6316a6212f921625cf95df2419d010280609d
Michael Howell <michael@notriddle.com>
2016-02-04 08:17:15 -0700

    Add a test case for the always-less broken comparator.

--- tests/test.rs
+++ tests/test.rs
@@ -5,13 +5,14 @@ extern crate itertools;
 use quickersort::{sort_by, insertion_sort, heapsort};
 use rand::{Rng, weak_rng};
 use itertools::Itertools;
+use std::cmp::Ordering::{Less, Greater};
 
 macro_rules! do_test_sort(
     ($sortfun:ident) => ({
         let cmp = |a: &usize, b: &usize| a.cmp(b);
         let cmp_rev = |a: &usize, b: &usize| b.cmp(a);
         for len in (4usize .. 250).step(5) {
-            for _ in (0isize .. 100) {
+            for _ in 0isize .. 100 {
                 let mut v = weak_rng().gen_iter::<u8>().take(len).map(|x| 10 + (x % 89) as usize)
                                         .collect::<Vec<usize>>();
                 let mut v1 = v.clone();
@@ -49,5 +50,23 @@ fn test_heapsort() {
 #[test]
 fn test_insertion_sort() {
     do_test_sort!(insertion_sort);
+}
+
+#[test]
+fn test_always_less() {
+    let mut v = vec![];
+    for _ in 0 .. 256 {
+        v.push(0u8);
+    }
+    sort_by(&mut v, &|_, _| Less);
+}
+
+#[test]
+fn test_always_greater() {
+    let mut v = vec![];
+    for _ in 0 .. 256 {
+        v.push(0u8);
+    }
+    sort_by(&mut v, &|_, _| Greater);
 }
 
