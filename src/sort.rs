93be0bdea8dc5f5f32063a86a45bd0dd1620c9cd
Michael Howell <michael@notriddle.com>
2017-01-20 11:51:44 -0700

    Update copyright notice

--- src/sort.rs
+++ src/sort.rs
@@ -1,5 +1,5 @@
 // (C) 2015 Viktor Dahl <pazaconyoman@gmail.com>
-// (C) 2015 Michael Howell <michael@notriddle.com>
+// (C) 2015-2017 Michael Howell <michael@notriddle.com>
 // This file is licensed under the same terms as Rust itself.
 
 use core::cmp::Ordering;
